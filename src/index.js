3183a9fa3c4e240bf8791915bda2745ad3796681
saliamanovich <saliamanovicha@gmail.com>
2022-07-24 13:50:50 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let steck = [];
+let mas = str.split('');
+
+for (let key of mas){
+  for (let i of bracketsConfig){
+    if (key===i[0] && key===i[1]){
+      if (steck[steck.length-1]===key){
+        steck.pop();
+      }
+      else {
+        steck.push(key);
+      }
+    }
+    else {
+      if (key===i[0]){
+        steck.push(key);
+      }
+      else if (key===i[1]){
+        if (i[0]===steck[steck.length-1])
+        steck.pop();
+        else {
+          steck.push(key);
+        }
+      }
+      else {
+        
+      }
+    }
+  }
+}
+  if (steck.length===0){
+    
+    return true;
+  }
+  else {
+       return false;
+  }
+  
 }
